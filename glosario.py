f306179322526f8192eb210f73ce9f9df494d577
Celia Hernandez <celia.a@alam.mx>
2026-08-25 11:45:12 -0600

    [UPD]: Se actualiza la forma de guardar las palabras del diccionario y se estas se guardan en el json

--- glosario.py
+++ glosario.py
@@ -1,24 +1,28 @@
-#Primero son las key(llaves) y luego los values(valores)
-print("GLOSARIO INGLÉS ESPAÑOL")
+import json
 
-#Almacena la palabra a buscar que ingresa el usuario
-busqueda = input("¿Qué palabra deseas buscar? ")
+with open("glosario.json") as f:
+    glosario = json.load(f)
+    #Primero son las key(llaves) y luego los values(valores)
+    print("GLOSARIO INGLÉS ESPAÑOL")
 
-#Guarda la información del glosario
-glosario = {'key' : 'llave', 'house' : 'casa', 'table' : 'mesa'}
+    #Almacena la palabra a buscar que ingresa el usuario
+    busqueda = input("¿Qué palabra deseas buscar? ")
 
-#Hace la busqueda de la palabra que le genero el usuario
-resultado = (glosario.get(busqueda))
+    #Hace la busqueda de la palabra que le genero el usuario
+    resultado = (glosario.get(busqueda))
 
-#Condición que busca la palabra si esta agregada o no y si desea agregarla.
-if  (resultado is None):
-    print("La palabra que buscas no está en el glosario, ¿Quieres agregarla?")
-    Respuesta = int(input(" 1-. Sí\n 2-. No\n"))
-    if(Respuesta == 1):
-        palabra_ingles = input("Escribe la palabra en inglés: ")
-        palabra_espanol = input("Escribe la traducción: ")
-        glosario[palabra_ingles] = palabra_espanol
-        print("¡La palabra fue agregada con éxito!")
+    #Condición que busca la palabra si esta agregada o no y si desea agregarla.
+    if  (resultado  is None):
+        print("La palabra que buscas no está en el glosario, ¿Quieres agregarla?")
+        Respuesta = int(input(" 1-. Sí\n 2-. No\n"))
+        if(Respuesta == 1):
+            #Guarda la traducción
+            palabra_espanol = input("Escribe la traducción: ")
+            #Es como si buscaramos dog: glosario["dog"] = "perro"
+            glosario[busqueda] = palabra_espanol
+            with open("glosario.json", "w") as e:
+                json.dump(glosario,e)
+                print("¡La palabra fue agregada con éxito!")
 
-else:
-    print(resultado)
+    else:
+        print(resultado)
