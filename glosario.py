4e1b75308675240c9f2cd6253da6739d1e0e04b1
Celia Hernandez <celia.a@alam.mx>
2026-08-25 17:10:57 -0600

    [UPD]: actualizacion de comentarios para saber el funcionamiento

--- glosario.py
+++ glosario.py
@@ -6,7 +6,7 @@ with open("glosario.json") as f:
     print("GLOSARIO INGLÉS ESPAÑOL")
 
     #Almacena la palabra a buscar que ingresa el usuario
-    busqueda = input("¿Qué palabra deseas buscar? ")
+    busqueda = input("¿Qué palabra deseas buscar? ").lower().strip()
 
     #Hace la busqueda de la palabra que le genero el usuario
     resultado = (glosario.get(busqueda))
@@ -16,13 +16,21 @@ with open("glosario.json") as f:
         print("La palabra que buscas no está en el glosario, ¿Quieres agregarla?")
         Respuesta = int(input(" 1-. Sí\n 2-. No\n"))
         if(Respuesta == 1):
+
             #Guarda la traducción
-            palabra_espanol = input("Escribe la traducción: ")
+            palabra_espanol = input("Escribe la traducción: ").lower().strip()
+
             #Es como si buscaramos dog: glosario["dog"] = "perro"
             glosario[busqueda] = palabra_espanol
+
+            #abre el .json y le damos la opción de escribir, que para nosotros es la variable e
             with open("glosario.json", "w") as e:
+
+                #Toma el contenido de glosario y lo escribe dentro del archivo e
                 json.dump(glosario,e)
                 print("¡La palabra fue agregada con éxito!")
+        else:
+            print("Hasta la proximaaaa, tutututututututu")
 
     else:
         print(resultado)
