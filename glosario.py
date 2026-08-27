0ca1d4eb8c01b68420b6c3320823765ecb2b713b
Celia Hernandez <celia.a@alam.mx>
2026-08-27 11:37:34 -0600

    [UPD]: Se actualizo la vista del glosario, ahora tiene un menu interactivo

--- glosario.py
+++ glosario.py
@@ -3,34 +3,47 @@ import json
 with open("glosario.json") as f:
     glosario = json.load(f)
     #Primero son las key(llaves) y luego los values(valores)
+
     print("GLOSARIO INGLÉS ESPAÑOL")
+    print("¿Qué deseas hacer?")
+    print("1.- Buscar una palabra")
+    print("2.- Ver todas las palabras")
+    print("3.- Agregar una palabra")
 
-    #Almacena la palabra a buscar que ingresa el usuario
-    busqueda = input("¿Qué palabra deseas buscar? ").lower().strip()
+    opcion = int(input(""))
+    print("Elegiste la opción: ",opcion)
 
-    #Hace la busqueda de la palabra que le genero el usuario
-    resultado = (glosario.get(busqueda))
-
-    #Condición que busca la palabra si esta agregada o no y si desea agregarla.
-    if  (resultado  is None):
-        print("La palabra que buscas no está en el glosario, ¿Quieres agregarla?")
-        Respuesta = int(input(" 1-. Sí\n 2-. No\n"))
-        if(Respuesta == 1):
-
-            #Guarda la traducción
-            palabra_espanol = input("Escribe la traducción: ").lower().strip()
-
-            #Es como si buscaramos dog: glosario["dog"] = "perro"
-            glosario[busqueda] = palabra_espanol
-
-            #abre el .json y le damos la opción de escribir, que para nosotros es la variable e
-            with open("glosario.json", "w") as e:
-
-                #Toma el contenido de glosario y lo escribe dentro del archivo e
-                json.dump(glosario,e)
-                print("¡La palabra fue agregada con éxito!")
-        else:
-            print("Hasta la proximaaaa, tutututututututu")
-
-    else:
-        print(resultado)
+    match opcion:
+        case 1:
+            print("BUSCAR UNA PALABRA:")
+            busqueda = input("¿Qué palabra deseas buscar? ").lower().strip()
+            resultado = (glosario.get(busqueda))
+            if (resultado is None):
+                print("La palabra que intentas buscar no esta en el glosario, ¿quieres agregarla?")
+                respuesta = int(input(" 1- Sí \n 2- No"))
+                if (respuesta == 1):
+                    palabra_espanol2 = input("Escriba la traducción: ")
+                    glosario[busqueda] = palabra_espanol2
+                    with open ("glosario.json","w") as i:
+                        json.dump(glosario,i)
+                        print("Traducción agregada con éxito :D")
+                elif (respuesta == 2):
+                    print("Okey, adios")
+            else:
+                print("La traducción de tu palabra es:", resultado)
+         #  print("La traducción de tu palabra es: ",resultado)
+        case 2:
+            print("VER TODAS LAS PALABRAS")
+            for clave, valor in glosario.items():
+                print(clave,valor)
+        case 3:
+            print ("AGREGAR UNA PALABRA")
+            palabra_ingles = input("Escribe la palabra en inglés: ").lower().strip()
+            if (palabra_ingles in glosario):
+                print("Esa palabra ya está en el glosario, por favor revisa bien :D")
+            else:
+                palabra_espanol = input("Escribe la traducción: ")
+                glosario[palabra_ingles] = palabra_espanol
+                with open("glosario.json","w") as e:
+                    json.dump(glosario,e)
+                    print("Palabra agregada con éxito")
